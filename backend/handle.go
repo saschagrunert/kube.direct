082555fedbc870f70792a81efbc0dcb9ad28ca22
Sascha Grunert <sgrunert@redhat.com>
2023-06-09 19:47:02 +0200

    Revert "Add basic grpc API"
    This reverts commit 5d5ab736462c423e911882bfd1f17ae2a4bd76ca.

--- backend/handle.go
+++ backend/handle.go
@@ -4,37 +4,19 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
-	"sync/atomic"
-
-	"function/internal/server"
 )
-
-var grpcServerDead atomic.Bool
-
-func init() {
-	srv, err := server.New()
-	if err != nil {
-		log.Fatalf("Unable to init gRPC server: %v", err)
-	}
-
-	go func() {
-		if err := srv.Serve(); err != nil {
-			log.Printf("Unable to serve gRPC: %v", err)
-			grpcServerDead.Store(true)
-		}
-	}()
-}
 
 // Handle an HTTP Request.
 func Handle(_ context.Context, res http.ResponseWriter, req *http.Request) {
-	if grpcServerDead.Load() {
-		log.Fatal("The gRPC server is not running any more, exiting")
-	}
+	/*
+	 * YOUR CODE HERE
+	 *
+	 * Try running `go test`.  Add more test as you code in `handle_test.go`.
+	 */
 
-	log.Print("Received request")
+	fmt.Println("Received request")
 	prettyPrint(req, os.Stdout) // echo to local output
 	prettyPrint(req, res)
 }
